66efa373ced5c54b90b3c240cb66518cef62490c
Liorh <Liorh@users.noreply.github.com>
2026-08-28 14:33:22 +0300

    Fix import androidx.compose.foundation.border for line chart in StatisticsScreen

--- app/src/main/java/com/avih6/vehiclecheck/ui/screens/StatisticsScreen.kt
+++ app/src/main/java/com/avih6/vehiclecheck/ui/screens/StatisticsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
